dddae1fcb11264da7de69f91bc5ddf9bb27dabc5
David Legrand <david.legrand@clever-cloud.com>
2024-03-25 17:56:19 +0100

    v: patches and binaries list
    - Add patches to bypass non working tests/build
    - vup.v/vself.v for package managers in files
    - Remove sandbox bypass
    - List binaries to install

---
+++ packages/dev-lang/v/files/vup.v
@@ -0,0 +1,4 @@
+println('v up is disabled for packaged versions of V.
+Use your package manager to update your V package instead.
+Alternatively, if you do want a more recent V version, just clone V from source,
+then follow the instructions here: https://github.com/vlang/v#installing-v-from-source')
